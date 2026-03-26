49b4a9fc069ec1766f4cbb2af31e2c3fdc33a263
Panda <dpa280706@gmail.com>
2026-03-26 01:18:44 +0100

    feat : Integration datas in Chinook schemas (my first dag)

--- dags/chinook-init.py
+++ dags/chinook-init.py
@@ -5,7 +5,7 @@ from datetime import datetime
 with DAG(
     dag_id="init_chinook_pipeline",
     start_date=datetime(2024, 1, 1),
-    schedule=None,
+    schedule="@once",
     catchup=False,
 ) as dag:
 
@@ -13,7 +13,6 @@ with DAG(
         task_id="create_schemas",
         conn_id="chinook_connection",
         sql="""
-        CREATE SCHEMA IF NOT EXISTS chinook;
         CREATE SCHEMA IF NOT EXISTS ods_chinook;
         CREATE SCHEMA IF NOT EXISTS dsa_chinook;
         CREATE SCHEMA IF NOT EXISTS dwh_chinook;
@@ -23,7 +22,7 @@ with DAG(
     load_chinook = SQLExecuteQueryOperator(
         task_id="load_chinook_sql",
         conn_id="chinook_connection",
-        sql="/opt/airflow/database/Chinook_PostgreSql.sql"
+        sql="data/Chinook_PostgreSql.sql"
     )
 
     create_schemas >> load_chinook
