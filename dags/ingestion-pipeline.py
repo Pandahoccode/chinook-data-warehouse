4fe5b079012fb70cbc9e5497823d0b23dc0d4d99
Panda <dpa280706@gmail.com>
2026-03-26 11:50:21 +0100

    refactor : Add magasin database

--- dags/ingestion-pipeline.py
+++ dags/ingestion-pipeline.py
@@ -3,7 +3,7 @@ from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
 from datetime import datetime
 
 with DAG(
-    dag_id="init_chinook_pipeline",
+    dag_id="ingestion_pipeline",
     start_date=datetime(2024, 1, 1),
     schedule="@once",
     catchup=False,
@@ -13,9 +13,11 @@ with DAG(
         task_id="create_schemas",
         conn_id="chinook_connection",
         sql="""
+        CREATE SCHEMA IF NOT EXISTS ods_magasin;
+        CREATE SCHEMA IF NOT EXISTS dsa_magasin;
         CREATE SCHEMA IF NOT EXISTS ods_chinook;
         CREATE SCHEMA IF NOT EXISTS dsa_chinook;
-        CREATE SCHEMA IF NOT EXISTS dwh_chinook;
+        CREATE SCHEMA IF NOT EXISTS datawarehouse;
         """
     )
 
@@ -25,4 +27,12 @@ with DAG(
         sql="data/Chinook_PostgreSql.sql"
     )
 
+    load_magasin = SQLExecuteQueryOperator(
+        task_id="load_magasin_sql",
+        conn_id="chinook_connection",
+        sql="data/Magasin_PostgreSql.sql"
+    )
+
+
     create_schemas >> load_chinook
+    create_schemas >> load_magasin
