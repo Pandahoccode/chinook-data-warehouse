7ca810d209c43daf3d327f3a7b0867929c93ebf4
Panda <dpa280706@gmail.com>
2026-06-07 22:34:20 +0200

    feat: add dbt_docker_operator and update profiles.yml for database connection

--- dags/dbt_operator.py
+++ dags/dbt_operator.py
@@ -2,21 +2,24 @@ from airflow.providers.docker.operators.docker import DockerOperator
 from docker.types import Mount
 import os
 
-WINDOWS_PATH="D:/3. Mes études/3.1 BUT SD/BUT 2 Phuc Anh/SEM_4 - DANG/chinook-data-warehouse-v2"
+WINDOWS_PATH = os.getenv(
+    "HOST_WORKSPACE_PATH",
+    "D:/04_Code-est-le-pain/401_PROJECTS/chinook-data-warehouse-v2",
+)
 
 def dbt_docker_operator(task_id: str, command: str) -> DockerOperator:
     return DockerOperator(
         task_id=task_id,
         image="ghcr.io/dbt-labs/dbt-postgres:1.9.0",
         command=command,
-        network_mode="bridge",
-        docker_url="unix://var/run/docker.sock",
+        network_mode="chinook-data-warehouse-v2_airflow-network",
+        docker_url="unix:///var/run/docker.sock",
         environment={
             "DBT_PROFILES_DIR": "/root/.dbt"
         },
         mounts=[
             Mount(
-                source=os.path.join(WINDOWS_PATH, "dbt/chinook_dbt"),
+                source=os.path.join(WINDOWS_PATH, "dbt", "chinook_dbt"),
                 target="/usr/app",
                 type="bind"
             ),
