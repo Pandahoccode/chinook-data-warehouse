9df25b62dac0828971577d6b42a1dee5284eb649
Panda <dpa280706@gmail.com>
2026-06-10 23:58:55 +0200

    refactor: clean up playlist structures and source schema profiles

--- dbt/chinook_dbt/models/dwh/dwh_bridge_playlist_track.sql
+++ dbt/chinook_dbt/models/dwh/dwh_bridge_playlist_track.sql
@@ -4,3 +4,5 @@ SELECT
     playlist_id,
     track_id
 FROM {{ ref('snapshot_chinook_playlist_track') }}
+WHERE dbt_valid_to IS NULL
+
--- dbt/chinook_dbt/models/dwh/dwh_dim_playlist.sql
+++ dbt/chinook_dbt/models/dwh/dwh_dim_playlist.sql
@@ -4,3 +4,5 @@ SELECT
     playlist_id,
     playlist_name AS name
 FROM {{ ref('snapshot_chinook_playlist') }}
+WHERE dbt_valid_to IS NULL
+
